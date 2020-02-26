34b2d49bd74c61380412c640479d6923a0745bae
brian <riceb53@gmail.com>
2020-02-25 21:18:29 -0500

    add options hash to employee class

--- employee.rb
+++ employee.rb
@@ -27,17 +27,16 @@
 
 # p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
 
-
 # class
 class Employee
   attr_reader :first_name, :last_name, :salary, :active
   attr_writer :first_name
   
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+  def initialize(input_options)
+    @first_name = input_options[:first_name]  
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
   def print_info
@@ -51,7 +50,14 @@ end
 
 
 
-employee1 = Employee.new("Danilo", "Campos", 80000, true)
+# employee1 = Employee.new({:first_name => "Danilo",:last_name => "Campos", :salary => 80000,:active => true})
+
+employee1 = Employee.new({
+  :first_name => "Danilo",
+  :last_name => "Campos",
+  :salary => 80000,
+  :active => true
+})
 p "salary pre raise"
 p employee1.print_info
 employee1.give_annual_raise
@@ -64,6 +70,6 @@ employee1.first_name = "Mark"
 p employee1.print_info
 
 
-employee2 = Employee.new("Majora", "Carter", 70000, false)
-# p employee2.print_info
+employee2 = Employee.new(first_name: "Majora", last_name: "Carter", salary: 70000, active: false)
+p employee2.print_info
 
